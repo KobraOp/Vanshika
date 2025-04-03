b175a231f5f5f81fe3d31cbea93336e06c69ff21
KobraOp <professionalramiz9517@gmail.com>
2025-04-03 22:47:50 +0530

    change

--- secureCode.py
+++ secureCode.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
+import simpleaudio as sa
 
 class Secure:
     def __init__(self):
@@ -15,6 +16,8 @@ class Secure:
                  text="A Cup Noodles will be charged 😋",
                  font=(30)).pack(anchor='s',pady=200)
         
+        self.audio_call()
+        
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
 
         self.root.mainloop()
@@ -23,6 +26,11 @@ class Secure:
         if messagebox.askokcancel("Exit", "Dekh le Bhai ko nahi khilayegi"):
             self.root.destroy()
             Secure()
+
+    def audio_call(self):
+        wave_obj = sa.WaveObject.from_wave_file("vanshika\\assests\\audio.wav")
+        play_obj = wave_obj.play()
+        play_obj.wait_done()
 
 if __name__ == "__main__":
     Secure()
